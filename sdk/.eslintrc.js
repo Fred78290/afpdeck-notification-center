d07e05dd1216a86bf8312abe07d50e7887222a64
Frederic BOLTZ <frederic.boltz@gmail.com>
2023-12-03 17:32:27 +0100

    FIX: Exclude test from distribution

--- sdk/.eslintrc.js
+++ sdk/.eslintrc.js
@@ -1,161 +1,142 @@
 module.exports = {
+  ignorePatterns: [
+    "src/tests",
+    "**/*.test.ts",
+    "**/*.spec.ts",
+    "**/jest.config.ts",
+  ],
   env: {
     browser: true,
     es6: true,
-    node: true
+    node: true,
   },
-  extends: [
-    'prettier'
-  ],
-  parser: '@typescript-eslint/parser',
+  extends: ["prettier"],
+  parser: "@typescript-eslint/parser",
   overrides: [
     {
-      files: ['*.ts', '*.tsx'],
+      files: ["*.ts", "*.tsx"],
       parserOptions: {
-        project: ['./tsconfig.json'],
-        sourceType: 'module'
+        project: ["./tsconfig.json"],
+        sourceType: "module",
       },
-    }
+    },
   ],
   plugins: [
-    'eslint-plugin-import',
-    'eslint-plugin-jsdoc',
-    '@typescript-eslint',
-    '@typescript-eslint/tslint'
+    "eslint-plugin-import",
+    "eslint-plugin-jsdoc",
+    "@typescript-eslint",
+    "@typescript-eslint/tslint",
   ],
   rules: {
-    indent: ['error', 4],
-    '@typescript-eslint/await-thenable': 'error',
-    '@typescript-eslint/consistent-type-assertions': 'error',
-    '@typescript-eslint/member-delimiter-style': [
-      'error',
+    indent: ["error", 4],
+    "@typescript-eslint/await-thenable": "error",
+    "@typescript-eslint/consistent-type-assertions": "error",
+    "@typescript-eslint/member-delimiter-style": [
+      "error",
       {
         multiline: {
-          delimiter: 'none',
-          requireLast: true
+          delimiter: "none",
+          requireLast: true,
         },
         singleline: {
-          delimiter: 'semi',
-          requireLast: false
-        }
-      }
+          delimiter: "semi",
+          requireLast: false,
+        },
+      },
     ],
-    '@typescript-eslint/naming-convention': 'error',
-    '@typescript-eslint/no-empty-function': 'error',
-    '@typescript-eslint/no-floating-promises': 'error',
-    '@typescript-eslint/no-misused-new': 'error',
-    '@typescript-eslint/no-unnecessary-qualifier': 'error',
-    '@typescript-eslint/no-unnecessary-type-assertion': 'error',
-    '@typescript-eslint/no-unused-expressions': [
-      'error',
+    "@typescript-eslint/naming-convention": "error",
+    "@typescript-eslint/no-empty-function": "error",
+    "@typescript-eslint/no-floating-promises": "error",
+    "@typescript-eslint/no-misused-new": "error",
+    "@typescript-eslint/no-unnecessary-qualifier": "error",
+    "@typescript-eslint/no-unnecessary-type-assertion": "error",
+    "@typescript-eslint/no-unused-expressions": [
+      "error",
       {
         allowTaggedTemplates: true,
-        allowShortCircuit: true
-      }
+        allowShortCircuit: true,
+      },
     ],
-    '@typescript-eslint/prefer-namespace-keyword': 'error',
-    '@typescript-eslint/quotes': [
-      'error',
-      'single',
+    "@typescript-eslint/prefer-namespace-keyword": "error",
+    "@typescript-eslint/quotes": [
+      "error",
+      "single",
       {
-        avoidEscape: true
-      }
+        avoidEscape: true,
+      },
     ],
-    '@typescript-eslint/semi': [
-      'error',
-      'never'
-    ],
-    '@typescript-eslint/triple-slash-reference': [
-      'error',
+    "@typescript-eslint/semi": ["error", "never"],
+    "@typescript-eslint/triple-slash-reference": [
+      "error",
       {
-        path: 'always',
-        types: 'prefer-import',
-        lib: 'always'
-      }
+        path: "always",
+        types: "prefer-import",
+        lib: "always",
+      },
     ],
-    '@typescript-eslint/tslint/config': [
-      'error',
+    "@typescript-eslint/tslint/config": [
+      "error",
       {
-        'rules': {
-          'strict-type-predicates': true,
-          whitespace: true
-        }
-      }
+        rules: {
+          "strict-type-predicates": true,
+          whitespace: true,
+        },
+      },
     ],
-    '@typescript-eslint/type-annotation-spacing': 'error',
-    '@typescript-eslint/unified-signatures': 'error',
-    'brace-style': [
-      'error',
-      '1tbs'
+    "@typescript-eslint/type-annotation-spacing": "error",
+    "@typescript-eslint/unified-signatures": "error",
+    "brace-style": ["error", "1tbs"],
+    "comma-dangle": "error",
+    curly: ["error", "multi-line"],
+    "eol-last": "error",
+    eqeqeq: ["error", "smart"],
+    "id-blacklist": [
+      "error",
+      "any",
+      "Number",
+      "number",
+      "String",
+      "string",
+      "Boolean",
+      "boolean",
+      "Undefined",
+      "undefined",
     ],
-    'comma-dangle': 'error',
-    'curly': [
-      'error',
-      'multi-line'
-    ],
-    'eol-last': 'error',
-    'eqeqeq': [
-      'error',
-      'smart'
-    ],
-    'id-blacklist': [
-      'error',
-      'any',
-      'Number',
-      'number',
-      'String',
-      'string',
-      'Boolean',
-      'boolean',
-      'Undefined',
-      'undefined'
-    ],
-    'id-match': 'error',
-    'import/no-deprecated': 'error',
-    'jsdoc/check-alignment': 'error',
-    'jsdoc/check-indentation': 'error',
-    'jsdoc/newline-after-description': 'off',
-    'new-parens': 'error',
-    'no-caller': 'error',
-    'no-cond-assign': 'error',
-    'no-constant-condition': 'error',
-    'no-control-regex': 'error',
-    'no-duplicate-imports': 'error',
-    'no-empty': 'error',
-    'no-eval': 'error',
-    'no-fallthrough': 'error',
-    'no-invalid-regexp': 'error',
-    'no-multiple-empty-lines': 'error',
-    'no-redeclare': 'error',
-    'no-regex-spaces': 'error',
-    'no-return-await': 'error',
-    'no-throw-literal': 'error',
-    'no-trailing-spaces': 'error',
-    'no-underscore-dangle': 'error',
-    'no-unused-labels': 'error',
-    'no-var': 'error',
-    'one-var': [
-      'error',
-      'never'
-    ],
-    'radix': 'error',
-    'space-before-function-paren': [
-      'error',
-      'always'
-    ],
-    'space-in-parens': [
-      'error',
-      'never'
-    ],
-    'spaced-comment': [
-      'error',
-      'always',
+    "id-match": "error",
+    "import/no-deprecated": "error",
+    "jsdoc/check-alignment": "error",
+    "jsdoc/check-indentation": "error",
+    "jsdoc/newline-after-description": "off",
+    "new-parens": "error",
+    "no-caller": "error",
+    "no-cond-assign": "error",
+    "no-constant-condition": "error",
+    "no-control-regex": "error",
+    "no-duplicate-imports": "error",
+    "no-empty": "error",
+    "no-eval": "error",
+    "no-fallthrough": "error",
+    "no-invalid-regexp": "error",
+    "no-multiple-empty-lines": "error",
+    "no-redeclare": "error",
+    "no-regex-spaces": "error",
+    "no-return-await": "error",
+    "no-throw-literal": "error",
+    "no-trailing-spaces": "error",
+    "no-underscore-dangle": "error",
+    "no-unused-labels": "error",
+    "no-var": "error",
+    "one-var": ["error", "never"],
+    radix: "error",
+    "space-before-function-paren": ["error", "always"],
+    "space-in-parens": ["error", "never"],
+    "spaced-comment": [
+      "error",
+      "always",
       {
-        'markers': [
-          '/'
-        ]
-      }
+        markers: ["/"],
+      },
     ],
-    'use-isnan': 'error'
-  }
-}
+    "use-isnan": "error",
+  },
+};
